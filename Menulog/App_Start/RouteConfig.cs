418c47291ad026fe775af06532ffe20752355c0d
upalr <shovon.forums@gmail.com>
2018-09-10 15:05:06 +1000

    define action for search

--- Menulog/App_Start/RouteConfig.cs
+++ Menulog/App_Start/RouteConfig.cs
@@ -13,6 +13,12 @@ namespace Menulog
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            // /Cuisine/french
+
+            routes.MapRoute("Cuisine",
+                "cuisine/{name}",
+                new { controller="Cuisine", action ="Search", name = UrlParameter.Optional});
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ Menulog/Controllers/CuisineController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Menulog.Controllers
+{
+    public class CuisineController : Controller
+    {
+        // GET: Cuisine
+        public ActionResult Search(string name = "french")   // TUT: we do not need Index() action here. so replaced with the Search() action
+        {
+            var message = Server.HtmlEncode(name);
+
+            return Content(message);
+        }
+    }
+}
