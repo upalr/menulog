ac2d49a9878f48f8aad8e14b2971e60a72c1fad6
upalr <shovon.forums@gmail.com>
2018-09-10 12:32:35 +1000

    start project menulog

---
+++ Menulog/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Menulog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Menulog.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            //ViewBag.Location = "Melbourne, Australia";   // Tut: Use of viewbag
+            var model = new AboutModel();
+            model.Name = "Upal";
+            model.Location = "Melbourne, Australia";
+
+            return View(model);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
