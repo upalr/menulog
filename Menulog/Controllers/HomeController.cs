2397c0a0eea050f985ffc0f8664fc29f30c9391f
upalr <shovon.forums@gmail.com>
2018-09-10 13:37:24 +1000

    configure route

--- Menulog/Controllers/HomeController.cs
+++ Menulog/Controllers/HomeController.cs
@@ -11,13 +11,19 @@ namespace Menulog.Controllers
     {
         public ActionResult Index()
         {
+            var controller = RouteData.Values["controller"]; //TUT RouteData is a data structure. building by the routing engine.
+            var action = RouteData.Values["action"];
+            var id = RouteData.Values["id"];
+            var message = String.Format("{0}::{1} {2}", controller, action, id);
+            ViewBag.message = message;
+
             return View();
         }
 
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-            //ViewBag.Location = "Melbourne, Australia";   // Tut: Use of viewbag
+            //ViewBag.Location = "Melbourne, Australia";   // TUT: Use of viewbag
             var model = new AboutModel();
             model.Name = "Upal";
             model.Location = "Melbourne, Australia";
