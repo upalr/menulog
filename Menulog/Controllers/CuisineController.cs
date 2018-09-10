5713b7d97f5faaf0f6bc677a121aa45fc79578f1
upalr <shovon.forums@gmail.com>
2018-09-10 17:21:27 +1000

    define action selectors

--- Menulog/Controllers/CuisineController.cs
+++ Menulog/Controllers/CuisineController.cs
@@ -8,13 +8,19 @@ namespace Menulog.Controllers
 {
     public class CuisineController : Controller
     {
-        // GET: Cuisine
+        [HttpPost]
         public ActionResult Search(string name = "french")   // TUT: according to the routes, we do not need Index() action here. so replaced with the Search() action
         {
             var message = Server.HtmlEncode(name);
 
-            //return Content(message);
-            return RedirectToAction("Index", "Home", new { name = name });
+            return Content(message);
+            //return RedirectToAction("Index", "Home", new { name = name });
+        }
+
+        [HttpGet]
+        public ActionResult Search()
+        {
+            return Content("Search!");
         }
     }
 }
