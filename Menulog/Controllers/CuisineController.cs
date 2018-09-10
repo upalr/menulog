585d9822688d566b0d7d739c0b0d95dd4ecfff80
upalr <shovon.forums@gmail.com>
2018-09-10 17:08:50 +1000

    define action results

--- Menulog/Controllers/CuisineController.cs
+++ Menulog/Controllers/CuisineController.cs
@@ -9,11 +9,12 @@ namespace Menulog.Controllers
     public class CuisineController : Controller
     {
         // GET: Cuisine
-        public ActionResult Search(string name = "french")   // TUT: we do not need Index() action here. so replaced with the Search() action
+        public ActionResult Search(string name = "french")   // TUT: according to the routes, we do not need Index() action here. so replaced with the Search() action
         {
             var message = Server.HtmlEncode(name);
 
-            return Content(message);
+            //return Content(message);
+            return RedirectToAction("Index", "Home", new { name = name });
         }
     }
 }
